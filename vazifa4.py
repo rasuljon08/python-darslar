ae7a28d69ca9c28afa77dc1f02dd0b5e6913113c
rasuljon08 <131454701+rasuljon08@users.noreply.github.com>
2023-04-21 21:21:27 +0500

    4-dars
    for key,value  in sorted(phython.items()):

---
+++ vazifa4.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  2 13:53:14 2023
+
+@author: rasuljon
+"""
+
+""" nom = float(input("Pleas erter an even nomber>> "))
+if nom%2:
+    print("This not even nomber")
+else:print("thank you")
+"""
+
+
+"""age = float(input("Enter your age>>  "))
+if age <5 or age > 60 :
+    print("You are free to enter")
+elif age <18 :
+    print("100000 so'm to enter you")
+elif age > 18 :
+    print("200000 so'm to enter you")"""
+    
+    
+""" nom_1 =float(input("pleas enter frist nomber>>  "))
+nom_2 = float(input("pleas enter second nomber>>  "))
+if nom_1 == nom_2 :
+    print("the nombers are even")
+elif nom_1 > nom_2 :
+    print(f"{nom_1}>{nom_2}")
+else :
+    print(f"{nom_1}<{nom_2}") """
+    
+    
+""" mahsulotlar = ['non','uzum','gusht','moy','olma']
+savat =[]
+bor_m = []
+yuq_m = []
+for m in range(5):
+    savat.append(input(f"Savatga {m+1} mahsulotni qushing>>  "))
+for mahsulot in savat:
+    if mahsulot in mahsulotlar:
+        bor_m.append(mahsulot)
+        #print(f"{mahsulot} dukonimizda bor")
+    else:
+       yuq_m.append(mahsulot)
+        #print(f"{mahsulot} dukonimizda yuq") 
+print(f"Dukonimizda {bor_m} mahsulotlar bor")
+print(f"Dukonimizda {yuq_m} mahsulotlar yuq ") """
+
+
+
+
+
+""" user_n = ["rasuljon","samir","abdulaziz","vasila",]
+
+login= input("Slect login>>  ")
+
+if login in  user_n :
+    print("This login is busy:\nChoose a new login ")
+    
+else:  print("welcome") """
+
+son = int(input("Enter any nomber>> "))
+
+for n in range(2,11):
+    if not (son%n):
+        print(f"{son} soni {n} ga qoldiqsiz bulinadi")
+        
+        
+        
+    
+
+    
+
