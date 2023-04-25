d96f4295b8959540369397cf9c3fde3d741c1b6e
rasuljon08 <131454701+rasuljon08@users.noreply.github.com>
2023-04-25 10:58:16 +0500

    for operatori bilan ishlar

---
+++ vazifa.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+"""davlatlar = ['aqsh','germaniya','egpt','ubekistan','saudia','indonesia']
+print ("Elementlar soni ;",len(davlatlar))
+print ( sorted(davlatlar))
+print(sorted(davlatlar, reverse=True))
+print(davlatlar)
+(davlatlar.sort())
+print(davlatlar)
+davlatlar.sort(reverse=True)
+print(davlatlar)
+
+juft_sonlar = list(range(120,1200,2))
+print(juft_sonlar)
+jami = sum(juft_sonlar)
+print(jami)
+print(max(juft_sonlar)-min(juft_sonlar))
+print(juft_sonlar[0:20])
+print(juft_sonlar[-20:])
+print(juft_sonlar[530:550])"""
+
+
+taomlar = ['somsa','osh','shurva','non','sut','asal']
+nonushta = taomlar[:]
+del nonushta[1]
+del nonushta[2]
+nonushta.append('olma')
+print(nonushta)
+print(taomlar)
+nonushta = tuple()
+nonushta[0]=('qaymoq va non')
---
+++ vazifa2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 11:35:46 2023
+
+@author: rasuljon
+"""
+
+"""frends = ['rasul','ali','olim','samir']
+for n in frends:
+    print(f"Asslomu alaykum {n} ")
+print(f"Kod {len(frends)} marta takrorlandi")
+"""
+"""
+for n in range(10,100,1):
+   print(f"{n} ning kubi {n**3} ga teng")
+"""
+ 
+"""kitoblar =[]
+for n in range(0,5):
+    kitoblar.append(input(f"{n+1}-Sevimli kitobingizni kiriting>>>  "))
+print(kitoblar)
+"""
+n_pepole = int(input("How many people did you to talk today?>>  " ))
+isimlar=[]
+for n in range(n_pepole):
+    isimlar.append(input(f"{n+1}  Who was it you talked to ?>>>  "))
+print(isimlar)
