7947bcba8e315263b45074a8e2bd95ee23f962f5
rasuljon08 <131454701+rasuljon08@users.noreply.github.com>
2023-04-27 11:17:19 +0500

    Python 🐍  vazifa

---
+++ lesson_#.py
@@ -0,0 +1,12 @@
+
+
+#narx = 3000.50
+#son =int(input("Nichita non olasiz:  "))
+
+#natija = narx * son
+#print("Umumiy summa:  ",natija)
+
+
+a = int(input("a sonni kiriting:  "))
+b = int(input("b sonni kiriting:  "))
+print(f"a-sonning qiymati {b}- teng\nb-sonning qiymati {a}-ga teng")
