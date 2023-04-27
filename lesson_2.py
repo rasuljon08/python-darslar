7cdddfa35c2750d8c18adf65dddfdd1d63723292
rasuljon08 <131454701+rasuljon08@users.noreply.github.com>
2023-04-27 11:17:59 +0500

    Python 🐍  vazifa 2

---
+++ lesson_2.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+a= int(input("birinchi sonni kiriting:  "))
+b= int(input("ikkinchi sonni kirirting:  "))
+d=int(input("uchinchi sonni kirting:  "))
+c= a+b
+r= c\d
+print("Natija:  ",c)
+print("Natija:  ",r)
