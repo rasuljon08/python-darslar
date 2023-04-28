f9598bb40057537317286f41b394948782e38686
rasuljon08 <131454701+rasuljon08@users.noreply.github.com>
2023-04-28 10:04:04 +0500

    Python 🐍  berilgan sonni oxirni raqamini topuvchi dastur

---
+++ lesson_4.py
@@ -0,0 +1,24 @@
+# # 1 kg limon a so'm b tiyin. masalan 10000so'm 20 tiyn:
+# a = int(input("so'm:  "))
+# b = int(input("tiyn:  "))
+# c = int(input("Kg:  "))
+# n= c*((a*100)+b)
+# print("so'm:  ",n//100)
+# print("tiyn:  ",n%100)
+
+# a = int(input("biron sonni kiriting"))
+# print(f"kirgizgan sonizni oxirgi raqami {a%10}")
+
+# a =input("Kiriting")
+# print(a[::-1].capitalize())
+
+
+# a=input("so`zlarni kiriting:")
+# a=a.split()
+# print(a[1],a[0])
+
+# a = (input("Uch xonali son kiriting:  "))
+
+# print(int(a[0])+int(a[1])+int(a[2]))
+
+
