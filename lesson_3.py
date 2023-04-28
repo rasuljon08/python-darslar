72bcc0f534c6c150dbdea4b7f87de5ecd30da106
rasuljon08 <131454701+rasuljon08@users.noreply.github.com>
2023-04-28 10:02:14 +0500

    Python 🐍  market uchun hisob dasturi

---
+++ lesson_3.py
@@ -0,0 +1,7 @@
+# 1 kg limon a so'm b tiyin. masalan 10000so'm 20 tiyn:
+a = int(input("so'm:  "))
+b = int(input("tiyn:  "))
+c = int(input("Kg:  "))
+n= c*((a*100)+b))
+print("so'm:  ",n\\100)
+print("tiyn:  ",n%100)
