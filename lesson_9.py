452e27c82eba7b116cf72cc462dbf67bfcd517e4
rasuljon08 <131454701+rasuljon08@users.noreply.github.com>
2023-05-08 13:37:30 +0500

    Python  🐍

---
+++ lesson_9.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 27 10:44:19 2023
+
+@author: rasuljon
+"""
+# n = int(input("Biron musbat son kiring"))
+# b =   2
+# for a in range(2,n,2):
+#     s= a+b
+#     print(f"{a}+{b}={s} ")
+
+
