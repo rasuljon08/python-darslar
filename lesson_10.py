b67f5424080c1f28f2ef1903092afad6a79cf3f9
rasuljon08 <131454701+rasuljon08@users.noreply.github.com>
2023-05-13 14:46:48 +0500

    Python 🐍

---
+++ lesson_10.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 27 11:15:41 2023
+
+@author: rasuljon
+"""
+
+# n =input("Butun son Kiriting")
+# while True :
+#     if n
+
+
+a = input("Qiymat kiriting: ")
+while not a.isdigit():
+    print("Xatolik")
+    a = input("Qiymat kiriting: ")
+print("rahmat")
